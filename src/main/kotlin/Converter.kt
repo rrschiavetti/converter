b3ec5814dc53b3f42c152fc90673e343eeb37feb
Ricardo Schiavetti <rickrp@gmail.com>
2024-03-03 11:34:31 +0100

    chore: Add basic tests

--- src/main/kotlin/Converter.kt
+++ src/main/kotlin/Converter.kt
@@ -1,6 +1,16 @@
 class Converter {
 
     fun convert(milliseconds: String): String {
+
+        try {
+            val millisecondsLong = milliseconds.toLong()
+            if (millisecondsLong < 0 ) {
+                throw InputIsNotAValidNumberException("Provided milliseconds must be a valid number greater than zero and less than ${Long.MAX_VALUE}")
+            }
+        } catch (e: NumberFormatException) {
+            throw InputIsNotAValidNumberException("Provided milliseconds must be a valid number greater than zero and less than ${Long.MAX_VALUE}")
+        }
+
         val millisecondsInt: Int = milliseconds.toInt()
 
         val result = millisecondsInt / 1000
@@ -8,4 +18,6 @@ class Converter {
         return result.toString() + " seconds"
 
     }
+
+    class InputIsNotAValidNumberException(message: String) : IllegalArgumentException(message)
 }
--- src/test/kotlin/ConverterTest.kt
+++ src/test/kotlin/ConverterTest.kt
@@ -1,3 +1,4 @@
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
@@ -6,13 +7,33 @@ class ConverterTest {
     private val sut = Converter()
 
     @Test
-    fun `should parse a valid string representation of milliseconds to seconds `() {
+    fun `should convert a valid string representation of milliseconds to seconds `() {
         sut.convert("10000") shouldBe "10 seconds"
     }
 
     @Test
     fun `should return seconds ignoring leftover milliseconds`() {
         sut.convert("10001") shouldBe "10 seconds"
+    }
+
+    @Test
+    fun `should throw InputNotValid when input is not a number`() {
+        shouldThrow<Converter.InputIsNotAValidNumberException> { sut.convert("AAA") }
+    }
+
+    @Test
+    fun `should return InputNotValid when input is less than zero`() {
+        shouldThrow<Converter.InputIsNotAValidNumberException> {sut.convert("-1")}
+    }
+
+    @Test
+    fun `should return InputNotValid when input is a decimal number`(){
+        shouldThrow<Converter.InputIsNotAValidNumberException> { sut.convert("10.1") }
+    }
+
+    @Test
+    fun `should return InputNotValid when input is greater than 9223372036854775807`() {
+        shouldThrow<Converter.InputIsNotAValidNumberException> {sut.convert("9223372036854775808")}
     }
 
 }
